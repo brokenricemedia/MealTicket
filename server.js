c9bb8ee97f126ec8d5af39b514f4bf8eb51a6eb0
kelichau <tchau2@outlook.com>
2018-02-06 14:13:55 -0500

    Initial server files

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/happy-choice"); //mongodb://localhost/fullstack-jeopardy
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');    
+}); 
+
+// If the connection throws an error
+connection.on('error', (err) => {  
+  console.log('Mongoose default connection error: ' + err);
+}); 
+
+app.use(bodyParser.json());
+
+
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/client/build/index.html')
+  })
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
+})
