d05d919aae462f73480352dc118696223d41e443
Juanita Pinkney <dev.juanitapinkney@gmail.com>
2018-02-07 23:20:29 -0500

    adds consumer and vendor views

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
+import './components/forms/index.css';
 import Forms from "./components/forms";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header">   
+          <h1 className="App-title">Welcome to Meal Ticket!</h1>
         </header>
         <Forms />
       </div>
