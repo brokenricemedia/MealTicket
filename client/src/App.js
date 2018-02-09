9356527ca9c58a7d4705aa6ea80a9642b496d40e
Brian Peace <bpeace71@gmail.com>
2018-02-09 13:37:07 -0500

    navbar complete

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,23 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
+//import NavBar from "./components/navbar"
 import WrappedDemo from "./components/food"
 import './components/forms/forms.css';
 import Forms from "./components/forms";
 import './utils/API.js'
+import Navbar from "./components/navbar"
 
 class App extends Component {
   render() {
     return (
       <Router>
       <div className="App">
-
+      <Navbar />
         <header className="App-header">   
           <h1 className="App-title">Welcome to Meal Ticket!</h1>
 
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <WrappedDemo />
         <Forms />
       </div>
