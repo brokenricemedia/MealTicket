5a01fc8b7790c00f52c0ec174a76a408be707f57
JBW <whitco670@jbw-mbp-215.attlocal.net>
2018-02-10 11:12:15 -0500

    experiementing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const chefRoutes = require("./chef");
+const loginRoutes = require("./login");
 
 // Book routes
 router.use("/chef", chefRoutes);
+router.use("/login", loginRoutes);
 
 module.exports = router;
