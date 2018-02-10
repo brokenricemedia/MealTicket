85ab4035f7c38d056432318dd7004aa1dc51129d
unknown <aznprox@hotmail.com>
2018-02-10 17:55:49 -0500

    login is querying databse.

--- routes/api/login.js
+++ routes/api/login.js
@@ -2,15 +2,16 @@ const router = require("express").Router();
 const db = require("../../models");
 
 
-// Matches with "/api/chef"
+// Matches with "/api/login"
 router.post("/", (req, res) => {
-	console.log(req.body);
-	db.Chef.find({
+		console.log(req.body);
+	db.Chef.findOne({
 		// email: {$eq:req.body.email},
 		// password:{$eq:req.body.password}
 		email: req.body.email,
 		password: req.body.password
-	}).then(dbModel => res.json(dbModel))
+	})
+	.then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
 })
 
