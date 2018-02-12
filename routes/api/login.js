0f232a242f207900d664b0e2ebbdfd11667b7b77
JBW <whitco670@lawn-128-61-14-80.lawn.gatech.edu>
2018-02-12 12:38:46 -0500

    seeded food

---
+++ routes/api/login.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const db = require("../../models");
+
+
+// Matches with "/api/login"
+router.post("/", (req, res) => {
+		console.log(req.body);
+	db.Chef.findOne({
+		// email: {$eq:req.body.email},
+		// password:{$eq:req.body.password}
+		email: req.body.email,
+	})
+	.then((dbChef) => {
+		//check if req.body.password is === dbChef.password
+
+		
+	})
+      .catch(err => res.status(422).json(err));
+})
+
+module.exports = router;
